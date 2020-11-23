b872cd1fdecae7887533ae4c9295c9772bc38d29
scurfin <scurfin@users.noreply.github.com>
2020-11-23 11:48:37 +0100

    Update build_all.sh
    Commented M5Stack

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -22,26 +22,26 @@ cp ./ESP32/output/homepoint_espgeneric_full.bin ./release
 cp ./ESP32/homepoint.bin ./release/homepoint_espgeneric_oat_update.bin
 
 # Prepare M5Stack
-cd ..
-rm -rf build
-mkdir build
-cd build
-cmake -DM5STACK=ON ../
-make -j 12
-make spiffs_spiffs_bin
-cd ../scripts
-mkdir M5Stack
-cp ../build/ota_data_initial.bin ./M5Stack/
-cp ../build/partition_table/partition-table.bin ./M5Stack/
-cp ../build/bootloader/bootloader.bin ./M5Stack/
-cp ../build/homepoint.bin ./M5Stack/
-cp ../build/spiffs.bin ./M5Stack
-cd M5Stack
-python ../merge_bin_esp.py --output_name homepoint_m5stack_full.bin --bin_path partition-table.bin ota_data_initial.bin bootloader.bin homepoint.bin spiffs.bin --bin_address 0x8000 0xd000 0x1000 0x10000 0x2b0000
-cd ..
-mkdir release
-cp ./M5Stack/output/homepoint_m5stack_full.bin ./release
-cp ./M5Stack/homepoint.bin ./release/homepoint_m5stack_ota_update.bin
+# cd ..
+# rm -rf build
+# mkdir build
+# cd build
+# cmake -DM5STACK=ON ../
+# make -j 12
+# make spiffs_spiffs_bin
+# cd ../scripts
+# mkdir M5Stack
+# cp ../build/ota_data_initial.bin ./M5Stack/
+# cp ../build/partition_table/partition-table.bin ./M5Stack/
+# cp ../build/bootloader/bootloader.bin ./M5Stack/
+# cp ../build/homepoint.bin ./M5Stack/
+# cp ../build/spiffs.bin ./M5Stack
+# cd M5Stack
+# python ../merge_bin_esp.py --output_name homepoint_m5stack_full.bin --bin_path partition-table.bin ota_data_initial.bin bootloader.bin homepoint.bin spiffs.bin --bin_address 0x8000 0xd000 0x1000 0x10000 0x2b0000
+# cd ..
+# mkdir release
+# cp ./M5Stack/output/homepoint_m5stack_full.bin ./release
+# cp ./M5Stack/homepoint.bin ./release/homepoint_m5stack_ota_update.bin
 
 cp readme.txt ./release
 zip -r homepoint_release.zip ./release
