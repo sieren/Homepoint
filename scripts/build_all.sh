6ef2f90c608a83ad715d773debe2f154f1992b19
Sieren <matthias@s-r-n.de>
2021-01-05 18:00:26 +0100

    Add AZTouch MOD Board
    Add OTA/Full Release files for AZTouch MOD Board.

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -21,6 +21,29 @@ mkdir release
 cp ./ESP32/output/homepoint_espgeneric_full.bin ./release
 cp ./ESP32/homepoint.bin ./release/homepoint_espgeneric_ota_update.bin
 
+# AZTouch MOD
+
+cd ..
+rm -rf build
+mkdir build
+cd build
+cmake -DBOARD=AZTOUCHMOD ../
+make -j 12
+make spiffs_spiffs_bin
+cd ../scripts
+mkdir ESP32
+cp ../build/ota_data_initial.bin ./ESP32/
+cp ../build/partition_table/partition-table.bin ./ESP32/
+cp ../build/bootloader/bootloader.bin ./ESP32/
+cp ../build/homepoint.bin ./ESP32/
+cp ../build/spiffs.bin ./ESP32
+cd ESP32
+python ../merge_bin_esp.py --output_name homepoint_aztouchmod_full.bin --bin_path partition-table.bin ota_data_initial.bin bootloader.bin homepoint.bin spiffs.bin --bin_address 0x8000 0xd000 0x1000 0x10000 0x2b0000
+cd ..
+mkdir release
+cp ./ESP32/output/homepoint_aztouchmod_full.bin ./release
+cp ./ESP32/homepoint.bin ./release/homepoint_aztouchmod_ota_update.bin
+
 # Prepare M5Stack
 cd ..
 rm -rf build
