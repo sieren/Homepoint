956415010d7decc7fc18c8f588ce3d5d2f9c53b0
Sieren <matthias@s-r-n.de>
2021-02-06 22:49:52 +0100

    Add M5Stack Core2 to Build Script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -66,5 +66,28 @@ mkdir release
 cp ./M5Stack/output/homepoint_m5stack_full.bin ./release
 cp ./M5Stack/homepoint.bin ./release/homepoint_m5stack_ota_update.bin
 
+# Prepare M5StackC2
+cd ..
+rm -rf build
+mkdir build
+cd build
+cmake -DBOARD=M5STACKCORE2 ../
+make -j 12
+make spiffs_spiffs_bin
+cd ../scripts
+mkdir M5StackCore2
+cp ../build/ota_data_initial.bin ./M5StackCore2/
+cp ../build/partition_table/partition-table.bin ./M5StackCore2/
+cp ../build/bootloader/bootloader.bin ./M5StackCore2/
+cp ../build/homepoint.bin .//
+cp ../build/spiffs.bin ./M5StackCore2
+cd M5Stack
+python ../merge_bin_esp.py --output_name homepoint_m5stackcore2_full.bin --bin_path partition-table.bin ota_data_initial.bin bootloader.bin homepoint.bin spiffs.bin --bin_address 0x8000 0xd000 0x1000 0x10000 0x2b0000
+cd ..
+mkdir release
+cp ./M5StackCore2/output/homepoint_m5stackcore2_full.bin ./release
+cp ./M5StackCore2/homepoint.bin ./release/homepoint_m5stackcore2_ota_update.bin
+
+
 cp readme.txt ./release
 zip -r homepoint_release.zip ./release
